c27f22908d742da087ef096cb72acfe07b45355b
Lucas Fraga <ti.lucasfraga@gmail.com>
2017-09-06 10:08:31 -0300

    Algoritmos de Recursividade e Iteratividade

---
+++ Recursividade vs Iteratividade/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/* Teste de recursividade */
+		System.out.println(Recursive.fatorial(6));
+		
+		/* Teste de iteratividade */
+		System.out.println(Iterative.fatorial(6));
+	}
+
+}
